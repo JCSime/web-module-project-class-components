1eedaba6ca8bbe0f6ecfea4e89bfe17d083de0e8
John S <johnc.sime@gmail.com>
2022-01-10 17:40:10 -0500

    Got the page title, input box, add btn, and clear btn setup, still need to fix toggle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,76 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
+import './style.css';
+
+const toDoArray = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            toDoArray: toDoArray
+        };
+    }
+    handleClearCompleted = ()=> {
+        this.setState({
+            ...this.state,
+            toDoArray: this.state.toDoArray.filter(item => {
+                return !item.handleClearCompleted;
+            })
+        });
+    }
+    handleAddItem = (itemName)=> {
+        const newItem = {
+            task: itemName,
+            id: Date.now(),
+            completed: false
+        };
+        this.setState({
+            ...this.state,
+            toDoArray: [...this.state.toDoArray, newItem]
+        });
+    }
+    handleToggleItem = (selectedItem) => {
+        this.setState({
+            ...this.state,
+            toDoArray: this.state.toDoArray.map(item => {
+                if(item.id === selectedItem.id) {
+                    return({
+                        ...item,
+                        completed: !item.completed
+                    })
+                } else {
+                    return item;
+                }
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <div className='header'>
+                  <h2>Welcome to your Todo App!</h2>
+                    <TodoForm handleAddItem={this.handleAddItem}/>
+                </div>
+                <TodoList handleToggleItem={this.handleToggleItem} toDoArray={this.state.toDoArray} />
+                <button onClick={this.handleClearCompleted} className='clear-btn'>Clear Completed</button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+
+import Item from "./Todo";
+
+const ToDoList = props => {
+    return (
+        <div className="todo-list">
+            {props.toDoArray.map(item => (
+                <Item handleToggleItem={props.handleToggleItem} key={item.id} item={item} />
+            ))}
+        </div>
+    )
+};
+
+export default ToDoList;
